3d6b18620ddfd1fa6728411135ea1bff11b9a981
yyubin <hazing120@gmail.com>
2024-12-13 15:18:40 +0900

    test: 게시글 crud 테스트 작성

--- src/main/java/org/durcit/be/security/util/SecurityUtil.java
+++ src/main/java/org/durcit/be/security/util/SecurityUtil.java
@@ -3,8 +3,11 @@ package org.durcit.be.security.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.durcit.be.system.exception.auth.NoAuthenticationInSecurityContextException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.durcit.be.system.exception.ExceptionMessage.NO_AUTHENTICATION_IN_SECURITY_CONTEXT_ERROR;
 
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -12,7 +15,7 @@ public class SecurityUtil {
     public static Long getCurrentMemberId() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || authentication.getName() == null) {
-            throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
+            throw new NoAuthenticationInSecurityContextException(NO_AUTHENTICATION_IN_SECURITY_CONTEXT_ERROR);
         }
         return Long.parseLong(authentication.getName());
     }
