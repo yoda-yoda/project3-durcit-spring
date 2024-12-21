187678e5dac1d5f2559776a10b60071f6570a111
yyubin <hazing120@gmail.com>
2024-12-21 20:18:02 +0900

    refactor: 팔로워들에게 푸시알람 전송시 같은 푸시 알람 큐 사용하도록 개선

--- src/main/java/org/durcit/be/post/service/impl/PostNotificationServiceImpl.java
+++ src/main/java/org/durcit/be/post/service/impl/PostNotificationServiceImpl.java
@@ -7,6 +7,7 @@ import org.durcit.be.post.dto.PostResponse;
 import org.durcit.be.post.service.PostNotificationService;
 import org.durcit.be.push.domain.Push;
 import org.durcit.be.push.domain.PushType;
+import org.durcit.be.push.dto.NotificationMessage;
 import org.durcit.be.push.service.PushService;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -22,17 +23,18 @@ public class PostNotificationServiceImpl implements PostNotificationService {
 
     public void notifyFollowers(PostResponse post, List<MemberFollowResponse> followers) {
         for (MemberFollowResponse follower : followers) {
-            PostNotificationMessage message = PostNotificationMessage.builder()
-                    .followerId(follower.getMemberId())
+            NotificationMessage notification = NotificationMessage.builder()
+                    .messageReceiver(follower.getMemberId())
                     .postId(post.getId())
                     .message(post.getAuthor() + "님이 새 글을 작성하였습니다!")
                     .build();
-            pushService.createPush(Push.builder()
-                            .pushType(PushType.FOLLOWER)
-                            .memberId(String.valueOf(follower.getMemberId()))
-                            .content(message.getMessage())
+            Push push = pushService.createPush(Push.builder()
+                    .pushType(PushType.FOLLOWER)
+                    .memberId(String.valueOf(follower.getMemberId()))
+                    .content(notification.getMessage())
                     .build());
-            rabbitTemplate.convertAndSend("postExchange", "post.notify", message);
+            notification.setId(push.getId());
+            rabbitTemplate.convertAndSend("notifyExchange", "notify.notify", notification);
         }
     }
 }
